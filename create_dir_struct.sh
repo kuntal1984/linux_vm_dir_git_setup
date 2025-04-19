ad19b7eb294cf64f596d04b18897fd391ae5e15a
kuntal1984 <74119702+kuntal1984@users.noreply.github.com>
2025-04-19 21:17:53 +0530

    Add files via upload

---
+++ create_dir_struct.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+proj_path='/home/kuntal_rs/projects'
+script_repos_dir='katacoda-scenarios'
+script_dir='scripts'
+script_fil='script.sh'
+git_repos_url='https://github.com/kuntal1984'
+arti_repos_dir='startup'
+arti_dir='artifacts'
+
+mkdir -p $proj_path
+mkdir -p $proj_path/$script_dir
+mkdir -p $proj_path/$arti_dir
---
+++ git_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo su -
+yum install git
+exit
+~         
